45bb619c2202349ac31199476d763d9e9461de75
Matt <>
2026-04-13 17:40:15 +0900

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@clovirtualfashion/components/styles/globals.css': fileURLToPath(new URL('./vendor/clovirtualfashion-components/dist/styles/globals.css', import.meta.url)),
+      '@clovirtualfashion/components': fileURLToPath(new URL('./vendor/clovirtualfashion-components/dist/index.es.js', import.meta.url)),
+      '@clovirtualfashion/foundation': fileURLToPath(new URL('./vendor/clovirtualfashion-foundation/dist/foundation.es.js', import.meta.url)),
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
